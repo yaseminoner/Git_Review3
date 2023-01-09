3f3f826d6818ad34780760c06d6e9dd58a7ed596
yaseminoner <114506530+yaseminoner@users.noreply.github.com>
2023-01-09 16:57:09 -0500

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,5 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        
+        //Some changes are made
+        
+        System.out.println("First Class");
     }
 }
